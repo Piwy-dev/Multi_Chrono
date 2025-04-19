1330df5475a1a00f99bc65eb1bb555e9cd5671a2
Pierre-Yves Haubruge <pierre-yves.haubruge@student.uclouvain.be>
2025-04-19 10:50:23 +0200

    disable start button

--- chrono/static/js/minuteur.js
+++ chrono/static/js/minuteur.js
@@ -10,13 +10,30 @@ function updateTimerDisplay(timerText, totalSeconds) {
     }
 }
 
-function startTimer(minutes, seconds, timerTextId) {
+function startTimer(minutes, seconds, teamId) {
     let totalSeconds = minutes * 6 + seconds;
 
     const interval = setInterval(() => {
         totalSeconds--;
-        updateTimerDisplay(document.getElementById(timerTextId), totalSeconds);
+        updateTimerDisplay(document.getElementById('timer-' + teamId), totalSeconds);
     }, 1000);
 
-    updateTimerDisplay(document.getElementById(timerTextId), totalSeconds);
+    updateTimerDisplay(document.getElementById('timer-' + teamId), totalSeconds);
+
+    // Disable the start button
+    const startButton = document.getElementById('start-' + teamId);
+    console.log(teamId)
+    console.log(startButton);
+    startButton.disabled = true;
+    startButton.style.display = 'none';
+
+    // Enable the stop button
+    const stopButton = document.getElementById('stop-' + teamId);
+    stopButton.disabled = false;
+    stopButton.style.display = 'block';
+    
+    // Enable the reset button
+    const resetButton = document.getElementById('reset-' + teamId);
+    resetButton.disabled = false;
+    resetButton.style.display = 'block';
 }
